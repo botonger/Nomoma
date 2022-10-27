1aed2d76f24c6b1c56fd866e8fa4340fc574127a
botonger <shinecheyenne@gmail.com>
2022-10-27 22:54:28 +0900

    ADD 매분기 1일 오전 9시 팀비 등록

--- src/main/java/com/nomoma/budget/svc/impl/IncomeServiceImpl.java
+++ src/main/java/com/nomoma/budget/svc/impl/IncomeServiceImpl.java
@@ -1,21 +1,28 @@
 package com.nomoma.budget.svc.impl;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.nomoma.budget.model.dto.IncomeDto;
 import com.nomoma.budget.model.entity.Income;
+import com.nomoma.member.model.dto.MemberDto;
 import com.nomoma.member.model.entity.Member;
 import com.nomoma.budget.repo.IncomeRepository;
 import com.nomoma.member.repo.MemberRepository;
 import com.nomoma.budget.svc.IncomeService;
+import com.nomoma.util.DateConverter;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RequiredArgsConstructor
+@Slf4j
 @Service
 public class IncomeServiceImpl implements IncomeService {
     private final IncomeRepository incomeRepository;
@@ -44,6 +51,22 @@ public class IncomeServiceImpl implements IncomeService {
 
         final Income saved = incomeRepository.save(IncomeDto.toEntity(incomeDto));
         return IncomeDto.toDto(saved);
+    }
+    @Scheduled(cron = "0 0 9 1 1/3 ? *")
+    public void saveQuarterlyIncome() {
+        log.info("===================================");
+        log.info("[Scheduler] : [saveQuarterlyIncome]");
+        log.info("[Execution Time] : " + DateConverter.currentDateTime());
+        log.info("===================================");
+        List<Member> members = memberRepository.findAll();
+        List<Income> incomes = new ArrayList<>();
+
+        members.forEach(member -> {
+            Income income = Income.builder().member(member).quarter(DateConverter.currentQuarter()).amount(200000).build();
+            incomes.add(income);
+        });
+
+        incomeRepository.saveAll(incomes);
     }
 
     //팀비 수정
